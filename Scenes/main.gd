fa8a98e3adf986d6af8bb6f1b5e81458312f9002
Eawell <138933958+Eawell@users.noreply.github.com>
2023-07-07 22:47:40 +0100

    Added stats that can change

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 const EVENT = preload("res://Scenes/event.tscn")
 
-var stats = {
-	'family': 0,
-	'friends': 0,
-	'work': 0
+var multiplier = 2
+@onready var stats = {
+	'family': 800,
+	'friends': 800,
+	'work': 800,
+	'energy': 800
 	
 	
 }
@@ -16,7 +18,9 @@ func _process(delta):
 	if Input.is_action_just_pressed("Spawn"):
 		spawn_event()
 	
-	
+	for type in global.TYPES:
+		get_node("Stats/"+type+"/Bar").value = stats[type]
+	$Stats/energy/Bar.value = stats['energy']
 	
 func spawn_event():
 	var event_instance = EVENT.instantiate()
@@ -29,3 +33,23 @@ func spawn_event():
 	if available.size() > 0:
 		event_instance.position = get_node('Calendar/Lane' + str(available[randi() % available.size()])).position + Vector2(0,50)
 		$Events.add_child(event_instance)
+
+
+
+func _on_timer_timeout():
+	var areas = $Calendar/Point_check.get_overlapping_areas()
+	for type in global.TYPES:
+		stats[type] -= 0.5 * multiplier
+		stats[type] = min(stats[type], 1000)
+		stats[type] = max(stats[type], 0)
+		stats['energy'] = min(stats['energy'], 1000)
+		stats['energy'] = max(stats['energy'], 0)
+	if areas.size() == 0:
+		stats['energy'] += multiplier
+	elif areas.size() == 1:
+		stats[areas[0].get_parent().type] += 3 * multiplier
+		stats['energy'] -= multiplier
+	else:
+		for area in areas:
+			stats[area.get_parent().type] -= 4 * multiplier
+			stats['energy'] -= multiplier
